d1545503bbe341cdc879dc15e8e31871aaeabee8
abcdef <gotqlc2670@naver.com>
2021-09-07 16:02:31 +0900

    DOM 제어, 조작

--- 연습.js
+++ 연습.js
@@ -1,11 +1,4 @@
+let total = 0;
+let n = 1;
 
-let num = 3;
-for(let i = 2; i < num ; i++) {
-    if (num % i === 0) {
-        console.log('소수가 아님');
-    }
-    else if(num < 55) {
-        break;
-    }
-    num++;
-}
+while (n)
