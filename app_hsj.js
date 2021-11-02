0ef9bf945aa8ca0436430e5e08259ea4c7a1e9ed
GUINNESS <hb743054@gamil.com>
2021-11-02 20:09:47 +0900

    [21.11.02] 이벤트 객체 연습

---
+++ app_hsj.js
@@ -0,0 +1,51 @@
+//================== 전역 변수 ==================//
+const budgetList = [
+    {
+        id : 1,
+        text : '식비',
+        amount : -100000
+    },
+    {
+        id : 2,
+        text : '통신비',
+        amount : -70000
+    }
+];
+
+
+//================== 함수 정의 ==================//
+
+var salaries = {
+    kim: 1000000,
+    park: 1600000,
+    lee: 1300000
+  };
+
+//   var total = salaries.kim + salaries.park + salaries.lee;
+
+
+
+
+
+ //data-id 값으로 배열을 탐색하여 인덱스를 리턴
+ function findIndexById(dataId) {
+
+    for(let i=0; i <todos.length; i++) {
+        if(todos[i].id === dataId) {
+            return i;
+        }
+    }
+    return null; //못찾았을 때 null리턴 혹은 -1
+
+}
+
+var total = 0;
+total += budgetList[i].amount
+total;
+//================== 메인 실행 ==================//
+
+(function () {
+
+
+
+})();
